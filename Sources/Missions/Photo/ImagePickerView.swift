d7788a791c60b042c85c3e9fe4661f311ed5a1e1
Zheng <aheze@getfind.app>
2023-06-02 17:57:09 -0700

    Start photos

---
+++ Sources/Missions/Photo/ImagePickerView.swift
@@ -0,0 +1,62 @@
+//
+//  File.swift
+//
+//
+//  Created by A. Zheng (github.com/aheze) on 6/2/23.
+//  Copyright © 2023 A. Zheng. All rights reserved.
+//
+    
+import SwiftUI
+
+/// from https://stackoverflow.com/a/75234559/14351818
+struct ImagePickerView: UIViewControllerRepresentable {
+    private var sourceType: UIImagePickerController.SourceType
+    private let onImagePicked: (UIImage) -> Void
+    
+    @Environment(\.presentationMode) private var presentationMode
+    
+    public init(sourceType: UIImagePickerController.SourceType, onImagePicked: @escaping (UIImage) -> Void) {
+        self.sourceType = sourceType
+        self.onImagePicked = onImagePicked
+    }
+    
+    public func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = self.sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    public func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(
+            onDismiss: { self.presentationMode.wrappedValue.dismiss() },
+            onImagePicked: self.onImagePicked
+        )
+    }
+    
+    public final class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        private let onDismiss: () -> Void
+        private let onImagePicked: (UIImage) -> Void
+        
+        init(onDismiss: @escaping () -> Void, onImagePicked: @escaping (UIImage) -> Void) {
+            self.onDismiss = onDismiss
+            self.onImagePicked = onImagePicked
+        }
+        
+        public func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+        ) {
+            if let image = info[.originalImage] as? UIImage {
+                self.onImagePicked(image)
+            }
+            self.onDismiss()
+        }
+
+        public func imagePickerControllerDidCancel(_: UIImagePickerController) {
+            self.onDismiss()
+        }
+    }
+}
--- Sources/Missions/Photo/Photo.swift
+++ Sources/Missions/Photo/Photo.swift
@@ -1,11 +1,10 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by A. Zheng (github.com/aheze) on 6/2/23.
 //  Copyright © 2023 A. Zheng. All rights reserved.
 //
-    
 
 import SwiftUI
 
@@ -13,27 +12,88 @@ import SwiftUI
 
 public struct PhotoMissionProperties: Hashable, Codable {
     public var sensitivity = Double(0.5)
+    public var imageData: Data?
+    public var featurePrintData: Data?
 
-    public init(sensitivity: Double = Double(0.5)) {
+    public init(sensitivity: Double = Double(0.5), imageData: Data? = nil, featurePrintData: Data? = nil) {
         self.sensitivity = sensitivity
+        self.imageData = imageData
+        self.featurePrintData = featurePrintData
     }
 }
 
 // MARK: - Mission properties view
 
- struct PhotoMissionPropertiesView: View {
+struct PhotoMissionPropertiesView: View {
     @Binding var properties: PhotoMissionProperties
+    @State var presentingCamera = false
 
     var body: some View {
         VStack(spacing: 24) {
-            MissionPropertiesGroupView(header: "Add customization controls here") {}
+            Button {
+                presentingCamera = true
+            } label: {
+                VStack(spacing: 24) {
+                    Image(systemName: "camera.shutter.button.fill")
+                        .font(.title2)
+                        .fontWeight(.bold)
+
+                    Text("Tap to take a photo")
+                        .font(.title3)
+                }
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 32)
+                .padding(.vertical, 48)
+                .frame(maxWidth: .infinity)
+                .background {
+                    RoundedRectangle(cornerRadius: 16, style: .continuous)
+                        .fill(.thinMaterial)
+                }
+                .overlay {
+                    RoundedRectangle(cornerRadius: 16, style: .continuous)
+                        .strokeBorder(Color.primary, style: .init(lineWidth: 0.25, lineCap: .round, dash: [5], dashPhase: 2))
+                        .opacity(0.5)
+                }
+            }
+            .buttonStyle(.plain)
+
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Take a photo of a prominent object.")
+
+                Text("Examples:")
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .dynamicVerticalPadding()
+            .dynamicHorizontalPadding()
+            .background {
+                RoundedRectangle(cornerRadius: 16, style: .continuous)
+                    .fill(.thinMaterial)
+            }
+//            .sheet(isPresented: $presentingCodeScanner) {
+//                SetupCodeScanner { result in
+//
+//                    switch result {
+//                    case .success(let result):
+//                        print("r: \(result) -> \(result.string)")
+//                        presentingCodeScanner = false
+//                        properties.codeString = result.string
+//                    case .failure(let error):
+//                        print("Error scanning code: \(error)")
+//                        self.error = error
+//                    }
+//                }
+//            }
+//
         }
+        .missionPropertiesInvalidReason(reason: properties.featurePrintData == nil ? "Take a photo first" : nil)
+        .dynamicHorizontalPadding()
+//        .errorAlert(error: $error)
     }
- }
+}
 
 // MARK: - Mission view
 
- struct PhotoMissionView: View {
+struct PhotoMissionView: View {
     @Environment(\.debugMode) var debugMode
     @Environment(\.missionCompletion) var missionCompletion
     @Environment(\.missionUserInteractionOccurred) var missionUserInteractionOccurred
@@ -47,11 +107,11 @@ public struct PhotoMissionProperties: Hashable, Codable {
             }
         }
     }
- }
+}
 
 // MARK: - Previews
 
- struct PhotoMissionPropertiesViewPreview: View {
+struct PhotoMissionPropertiesViewPreview: View {
     @State var properties = PhotoMissionProperties()
 
     var body: some View {
@@ -61,16 +121,16 @@ public struct PhotoMissionProperties: Hashable, Codable {
         }
         .background(Color(uiColor: .systemGroupedBackground))
     }
- }
+}
 
- struct PhotoMissionPropertiesViewPreviewProvider: PreviewProvider {
+struct PhotoMissionPropertiesViewPreviewProvider: PreviewProvider {
     static var previews: some View {
         PhotoMissionPropertiesViewPreview()
     }
- }
+}
 
- struct PhotoMissionView_Previews: PreviewProvider {
+struct PhotoMissionView_Previews: PreviewProvider {
     static var previews: some View {
         PhotoMissionView(properties: .init())
     }
- }
+}
