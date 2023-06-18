2615dccf0ce94ce1bc5263f096f73de7a2928fdd
Zheng <aheze@getfind.app>
2023-06-18 00:47:04 -0700

    Remove dependencies

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/aheze/Prism", branch: "main"),
-         .package(path: "/Users/aheze/Desktop/Code/MidnightApp/Resources/CodeScanner")
+         .package(url: "https://github.com/aheze/CodeScanner", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
